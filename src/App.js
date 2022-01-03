b83d07d04bf58116438a5614b4bc2791c10ee576
Zankar1992 <zankar.logicalstreet@gmail.com>
2022-01-03 19:17:29 +0530

    registration form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [userRegistration, setUserRegistration] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: ""
+  });
+
+  const [records, setRecords] = useState([]);
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(value, name);
+
+    setUserRegistration({ ...userRegistration, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newRecords = { ...userRegistration, id: new Date().getTime().toString() }
+    console.log(records);
+    setRecords([...records, newRecords]);
+    console.log(records)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Fullname</label>
+          <input type="text" autoComplete="off"
+            value={userRegistration.username}
+            onChange={handleInput}
+            name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" autoComplete="off"
+            value={userRegistration.email}
+            onChange={handleInput}
+            name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input type="number" autoComplete="off"
+            value={userRegistration.phone}
+            onChange={handleInput}
+            name="phone" id="phone" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" autoComplete="off"
+            value={userRegistration.password}
+            onChange={handleInput}
+            name="password" id="password" />
+        </div>
+
+        <button type="submit">Registration</button>
+      </form>
+      <div>
+        {
+          records.map((curElem) => {
+            const {id,username,email,phone,password} = curElem;
+            return (
+              <div className="showDataStyle" key={curElem.id}>
+                <p>{username}</p>
+                <p>{email}</p>
+                <p>{phone}</p>
+                <p>{password}</p>
+              </div>  
+            )
+          })
+        }
+      </div>
+    </>
+  )
 }
 
 export default App;
